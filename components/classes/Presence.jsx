50287261e80b68dbba8bd1fab6be5c30967f5d30
Syahrul Safarila <57931052+SySafarila@users.noreply.github.com>
2022-04-19 22:40:23 +0700

    Update Presence.jsx

--- components/classes/Presence.jsx
+++ components/classes/Presence.jsx
@@ -1,21 +1,48 @@
-import { deleteDoc, doc, getFirestore, onSnapshot } from "firebase/firestore";
-import { useEffect } from "react";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  getFirestore,
+  onSnapshot,
+  query,
+  where,
+} from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { useRecoilValue } from "recoil";
+import { UserState } from "../RecoilState";
 
 const Presence = (props) => {
+  const [userCheckin, setUserCheckoin] = useState(false);
+
   const db = getFirestore();
+  const user = useRecoilValue(UserState);
   let presence = props.presence;
   let { uid } = presence;
 
   useEffect(() => {
     console.log(`Presence : mounted ${uid}`);
-    // const unsub = onSnapshot(doc(db, "presences", uid), (doc) => {
-    //   console.count(uid);
-    // });
 
-    // return () => {
-    //   console.log(`unsub ${uid}`);
-    //   unsub();
-    // };
+    const q = query(
+      collection(db, "presenceTypes"),
+      where("presence_id", "==", uid),
+      where("user_id", "==", user?.uid)
+    );
+
+    const unsubs = onSnapshot(q, (querySnapshot) => {
+      let arr = [];
+      querySnapshot.forEach((doc) => {
+        arr.push({ ...doc.data(), uid: doc.id });
+      });
+      if (arr.length > 0) {
+        setUserCheckoin(arr[0]);
+      }
+    });
+
+    return () => {
+      unsubs();
+    };
   }, []);
 
   const deletePresence = async () => {
@@ -30,18 +57,67 @@ const Presence = (props) => {
     }
   };
 
-  const checkin = (type) => {
+  const checkin = async (type) => {
     console.log(`checkin for ${type}`);
-  }
+
+    const q = query(
+      collection(db, "presenceTypes"),
+      where("presence_id", "==", uid),
+      where("user_id", "==", user?.uid)
+    );
+
+    try {
+      const querySnapshot = await getDocs(q);
+      let arr = [];
+      querySnapshot.forEach((doc) => {
+        arr.push({ ...doc.data(), uid: doc.id });
+      });
+
+      if (arr.length > 0) {
+        await deleteDoc(doc(db, "presenceTypes", arr[0].uid));
+        const DocRef = await addDoc(collection(db, "presenceTypes"), {
+          user_id: user?.uid,
+          presence_id: uid,
+          type: type,
+          class_id: presence.class_uid,
+          created_at: new Date().getTime(),
+        });
+      } else {
+        const DocRef = await addDoc(collection(db, "presenceTypes"), {
+          user_id: user?.uid,
+          presence_id: uid,
+          type: type,
+          class_id: presence.class_uid,
+          created_at: new Date().getTime(),
+        });
+      }
+    } catch (er) {
+      console.log(er);
+    }
+
+    // try {
+    //   const DocRef = await addDoc(collection(db, "presenceTypes"), {
+    //     user_id: user?.uid,
+    //     presence_id: uid,
+    //     type: type,
+    //     class_id: presence.class_uid,
+    //     created_at: new Date().getTime(),
+    //   });
+    // } catch (er) {
+    //   console.log(er);
+    // }
+  };
 
   return (
-    <div>
-      <p>{presence.message}</p>
-      <button onClick={() => checkin('hadir')}>Hadir</button>
-      <button onClick={() => checkin('izin')}>izin</button>
-      <button onClick={() => checkin('sakit')}>sakit</button>
+    <>
+      <p>
+        {presence.message} | {userCheckin ? userCheckin.type : ""}
+      </p>
+      <button onClick={() => checkin("hadir")}>Hadir</button>
+      <button onClick={() => checkin("izin")}>izin</button>
+      <button onClick={() => checkin("sakit")}>sakit</button>
       {props.isAdmin ? <button onClick={deletePresence}>hapus</button> : null}
-    </div>
+    </>
   );
 };
 
